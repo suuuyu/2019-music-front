321843d3d9233f6c631b13980101e198e63f7f08
IdealLifeTJU <39995424+IdealLifeTJU@users.noreply.github.com>
2019-06-05 20:03:14 +0800

    歌单详情页完成
    未完成部分：歌单封面的引入，播放全部按钮功能的完成

--- src/components/detail/songListDetail.vue
+++ src/components/detail/songListDetail.vue
@@ -8,33 +8,48 @@
 				</i-col>
 				<i-col span="14" class="songListText">
 					<div class="title">{{songList.songlistname}}</div>
-					<div class="subTitle">创建者：{{user.username}}</div>
+					<div class="subTitle">创建者：{{songListCreater.username}}</div>
 					<div class="text">
 						<Row>
 					   <i-col span="12">创建时间：{{songList.createtime}}</i-col>
 					  </Row>
 						<Row class="buttonGroup">
 							<i-col span="8"><button class="btn btn-default-large myButton" type="button">播放全部</button></i-col>							
-							<i-col span="8"><button class="btn btn-default-large myButton" type="button">收藏</button></i-col>
+							<i-col span="8"><button class="btn btn-default-large myButton" type="button" v-on:click="keepSongList" 
+							v-bind:class="{keepedButton:isKeeped}">{{isKeeped?'取消收藏':'收藏'}}</button></i-col>
 						</Row>
 					</div> 
 				</i-col>
 			</Row>
 		</div>
 		</Layout>
+		<Layout>
+			<div>
+        <Tabs value="name1" @on-click="handle">
+            <TabPane label="歌曲" name="name1" class="real-panel"><mylikesong :song="songs"></mylikesong></TabPane>
+        </Tabs>
+    </div>
+		</Layout>
   </div>
 </template>
 <script>
 import { AXIOS } from '../../http/http';
+import mylikesong from '../profile/mylike-song';
 export default{
   name:"songListDetail",
   data(){
     return{
       songListId:String,
       songList:{},
-      user:{},
+			songListCreater:{},
+			songs:[],
+			isKeeped:false,
+			userId:String,
     }
-  },
+	},
+	components:{
+		mylikesong,
+	},
   methods:{
     getSongListInfo:function(){
       AXIOS.get('/getSongListById',{
@@ -49,8 +64,19 @@ export default{
         this.getCreatedUserName();
       }).catch(response=>{
         console.log(response);
-      });
-    },
+			});
+		},
+		getSongsInList:function(){
+			AXIOS.get('/getSongsInList',{
+				params:{
+					songListId:this.songListId,
+				}
+			}).then(response=>{
+				this.songs = response.data;
+			}).catch(response=>{
+				console.log(response);
+			});
+		},
     getCreatedUserName:function(){
       AXIOS.get('/getUser',{
         params:{
@@ -58,16 +84,131 @@ export default{
         }
       }).then(response=>{
         console.log(response.data);
-        this.user = response.data;
+        this.songListCreater = response.data;
       }).catch(response=>{
         console.log(response);
       });
-    }
-  },
+		},
+	  keepSongList:function(){
+			console.log(this.isKeeped);
+			if(!this.isKeeped){
+				AXIOS.post('/keepSongListPost',this.$qs.stringify({
+					'userId':this.userId,
+					'songListId':this.songListId,
+				}),
+				{
+					headers:{
+						'Content-Type':'application/x-www-form-urlencoded',
+						'Access-Control-Allow-Origin':'http://localhost:8081',
+					}
+				}).then(response=>{
+					if(response.data){
+						this.isKeeped = true;
+						this.$alert('收藏成功！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+					}else{
+						this.$alert('收藏失败！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+					}
+				}).catch(response=>{
+					console.log(response);
+					this.$alert('收藏失败！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+				});
+			}else{
+				AXIOS.post('/cancelKeepSongList',this.$qs.stringify({
+					'userId':this.userId,
+					'songListId':this.songListId,
+				}),
+				{
+					headers:{
+						'Content-Type':'application/x-www-form-urlencoded',
+						'Access-Control-Allow-Origin':'http://localhost:8081',
+					}
+				}).then(response=>{
+					if(response.data){
+						this.isKeeped = false;
+						this.$alert('取消收藏成功！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+					}
+					else{
+						this.$alert('取消收藏失败！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+					}
+				}).catch(response=>{
+					console.log(response);
+					this.$alert('取消收藏失败！','提示',{
+							confirmButtonText:'确定',
+							callback:action=>{
+								this.$message({
+									type:'info',
+									message:`action: ${ action }`
+								});
+							}
+						});
+				});
+			}
+		},
+		judgeIfKeeped:function(){
+			AXIOS.get('/isSonglistSaved',{
+				params:{
+					userid:this.userId,
+					songlistid:this.songListId,
+				}
+			}).then(response=>{
+				console.log(response.data);
+				if(response.data=="0"){
+					this.isKeeped = false;
+				}else{
+					this.isKeeped = true;
+				}
+			}).catch(response=>{
+				console.log(response.data);
+			})
+		}
+	},
   mounted(){
-    this.songListId = this.$route.params.songListID;
-    this.getSongListInfo();
-  },
+		this.songListId = this.$route.params.songListID;
+		this.userId = sessionStorage.getItem("userid");
+		this.getSongListInfo();
+		this.getSongsInList();
+		this.judgeIfKeeped();
+	},
 }
 </script>
 <style scoped>
@@ -197,5 +338,9 @@ export default{
 	overflow: hidden;
 	text-overflow: ellipsis;
 	max-width: 90%
+}
+.keepedButton{
+	background:skyblue;
+	font:whitesmoke;
 }
 </style>
